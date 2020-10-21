e876ec24f1de11fb877edf12f24f53466b7bf30b
THAT BOY <zhu.thatboy@outlook.com>
2020-10-21 11:02:09 +0800

    运行通过

--- ORB-SLAM2/app/main.cpp
+++ ORB-SLAM2/app/main.cpp
@@ -4,7 +4,7 @@ using namespace ORB_SLAM2;
 int main()
 {
 	// 路径需要修改！！
-	System orbslam("../Run/Vocabulary/ORBvoc.txt", "../Run/Setting/KITTI03.yaml", System::eSensor::STEREO);
+	System orbslam("Vocabulary/ORBvoc.txt", "Examples/Stereo/KITTI03.yaml", System::eSensor::STEREO);
 	system("pause");
 	return 0;
 }
